5e7ba1acafbac3cec4145ce385905f05c81b1dad
ajloveslily14 <ajloveslily@gmail.com>
2018-10-25 20:02:47 -0700

    add example config

---
+++ exampleconfig.lua
@@ -0,0 +1,32 @@
+--[[
+	CONFIG FILE FOR PIN BOT, FORMATTED AS SHOWN BELOW
+]]
+
+config = {}
+
+
+config.servers = {
+	
+	['239599630805893121'] = {
+
+		adminRoles = {
+		
+			['504807141580865551'] = true,
+			['504808768488341505'] = true
+
+		},
+
+		pinChannel = '504807285923774468'
+	},
+
+	['505200471909335045'] = {
+
+		adminRoles = {
+			['505201887281414155'] = true
+		},
+
+		pinChannel = '505202070840934404'
+	}
+}
+
+return config
